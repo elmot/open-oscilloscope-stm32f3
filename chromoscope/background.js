af2e94a6fadf4533afeedc26428e9d753f746165
elmot73 <elijah.mot@gmail.com>
2016-01-24 16:52:09 +0200

    UI improvements

--- chromoscope/background.js
+++ chromoscope/background.js
@@ -1,11 +1,11 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    innerBounds: {
-      width: 670,
-      height: 350,
-      minWidth: 670,
-      minHeight: 350
-    },
-    id: "ChromeApps-Sample-USB-DeviceInfo"
-  });
+    chrome.app.window.create('index.html', {
+        innerBounds: {
+            width: 1024,
+            height: 750,
+            minWidth: 480,
+            minHeight: 350
+        },
+        id: "ChromeApps-Oscilloscope"
+    });
 });
--- chromoscope/ui.js
+++ chromoscope/ui.js
@@ -14,7 +14,7 @@ function drawData(data) {
     canvasCtx.fillRect(0, 0, canvasCtx.canvas.width, canvasCtx.canvas.height);
     for (var j = 0; j < data.length; j++) {
         var array = data[j];
-        if(array == null) continue;
+        if (array == null) continue;
         canvasCtx.strokeStyle = colors[j];
         canvasCtx.beginPath();
         var zx = canvasCtx.canvas.width / array.length;
@@ -29,21 +29,6 @@ function drawData(data) {
     }
 }
 
-function setParamFromInput(event) {
-    var elm = event.srcElement;
-    setParam(elm.name, elm.value)
-}
-
-function wheelSelect(event) {
-    if (event.srcElement.options) {
-        var idx = event.srcElement.selectedIndex;
-        if (idx > 0 && event.wheelDelta < 0) idx--;
-        else if (event.wheelDelta > 0 && idx + 1 < event.srcElement.options.length) idx++;
-        event.srcElement.selectedIndex = idx;
-    }
-    setParamFromInput(event)
-}
-
 function scanControls() {
     var nodeList = document.querySelectorAll(".paramInput");
     for (var i = 0; i < nodeList.length; i++) {
@@ -53,14 +38,82 @@ function scanControls() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    var nodeList = document.querySelectorAll(".paramInput");
-    for (var i = 0; i < nodeList.length; i++) {
-        nodeList[i].addEventListener("change", setParamFromInput);
+    function _addListener(selector, event, f) {
+        var list = document.querySelectorAll(selector);
+        for (var i = 0; i < list.length; i++) {
+            list[i].addEventListener(event, f);
+        }
     }
-    nodeList = document.querySelectorAll(".scrollSelect");
-    for (i = 0; i < nodeList.length; i++) {
-        nodeList[i].addEventListener("wheel", wheelSelect);
+
+    function setParamFromInput(event) {
+        var elm = event.srcElement;
+        console.log(elm.name + ":" +  elm.value);//todo remove
+        var nodeList = document.querySelectorAll(".input-value[for='" + elm.name + "']")
+        for(var i = 0; i < nodeList.length; i++) nodeList[i].innerHTML = elm.value
+        setParam(elm.name, elm.value)
     }
+
+    function wheelSelect(event) {
+        if (event.srcElement.options) {
+            var idx = event.srcElement.selectedIndex;
+            if (idx > 0 && event.wheelDelta > 0) idx--;
+            else if (event.wheelDelta < 0 && idx + 1 < event.srcElement.options.length) idx++;
+            event.srcElement.selectedIndex = idx;
+            event.srcElement.dispatchEvent(new Event("change"))
+        }
+
+    }
+
+    function wheelChange(event) {
+        if (event.srcElement.value) {
+            var v = event.srcElement.value - event.wheelDelta;
+            if(v < event.srcElement.min) v = event.srcElement.min;
+            if(v > event.srcElement.max) v = event.srcElement.max;
+            event.srcElement.value = v;
+            event.srcElement.dispatchEvent(new Event("change"))
+        }
+
+    }
+
+    function pickVertical(event) {
+        var elm = event.srcElement;
+
+        function verticalPickClick(event) {
+            cancelPick();
+            console.log(event)
+        }
+
+        function cancelPick() {
+            elm.classList.remove("pressed");
+            canvasCtx.canvas.classList.remove("aiming");
+            canvasCtx.canvas.removeEventListener("click", verticalPickClick)
+        }
+
+
+        if (elm.classList.contains("pressed")) {
+            cancelPick();
+        } else {
+            elm.classList.add("pressed");
+            canvasCtx.canvas.classList.add("aiming");
+            canvasCtx.canvas.addEventListener("click", verticalPickClick)
+        }
+    }
+
+    function inputReset(event) {
+        var elm = event.srcElement;
+        var valueElement = document.getElementById(elm.getAttribute("for"));
+        if(valueElement != null)
+        {
+            valueElement.value = elm.value;
+            valueElement.dispatchEvent(new Event("change"))
+        }
+    }
+
+    _addListener(".paramInput", "change", setParamFromInput);
+    _addListener(".wheelSelect", "wheel", wheelSelect);
+    _addListener(".vertical-picker", "click", pickVertical);
+    _addListener(".inputReset", "click", inputReset);
+    _addListener(".wheelChange", "wheel", wheelChange);
     initialDeviceDetect();
 });
 
