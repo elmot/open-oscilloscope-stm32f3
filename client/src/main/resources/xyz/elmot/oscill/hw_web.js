552408dcf02e5417aea63888384c31596862e50d
elmot <me@elmot.xyz>
2017-03-14 22:19:30 +0200

    JS optimization

--- client/src/main/resources/xyz/elmot/oscill/hw_web.js
+++ client/src/main/resources/xyz/elmot/oscill/hw_web.js
@@ -1,4 +1,27 @@
 var frameParam = {w: 2047, h: 4096};
+var oReq = new XMLHttpRequest();
+oReq.onerror = function (oEvent) {
+    document.getElementById("device-info").innerHTML = "<em>Communication error</em>";
+};
+oReq.ontimeout = function (oEvent) {
+    document.getElementById("device-info").innerHTML = "<em>Communication timeout</em>";
+};
+oReq.responseType = "arraybuffer";
+oReq.onload = function (oEvent) {
+  var arrayBuffer = oReq.response; // Note: not oReq.responseText
+  if (arrayBuffer && arrayBuffer.byteLength > 2) {
+    var dView = new DataView(arrayBuffer);
+    var header = dView.getUint16(0, true);
+
+    var shortArray = new Uint16Array(arrayBuffer.byteLength / 2 - 1);
+    for(var i = 0; i < shortArray.length;i++)
+        shortArray[i] = dView.getUint16(i * 2 + 2, true);
+    if(shortArray.length >0) {
+        drawData([shortArray]);
+    }
+    document.getElementById("device-info").innerHTML = "";
+  }
+};
 
 /**
  @type {CanvasRenderingContext2D}
@@ -9,44 +32,21 @@ var frameParam = {w: 2047, h: 4096};
 
 
 function setParam(name, value) {
-    var oReq = new XMLHttpRequest();
-    oReq.open("POST", "/cmd", true);
-    oReq.onerror = function (oEvent) {
+var cReq = new XMLHttpRequest();
+    cReq.onerror = function (oEvent) {
         document.getElementById("device-info").innerHTML = "<em>Communication error</em>";
     };
-    oReq.ontimeout = function (oEvent) {
+    cReq.ontimeout = function (oEvent) {
         document.getElementById("device-info").innerHTML = "<em>Communication timeout</em>";
     };
-
-    oReq.send(name + "=" + value);
+    cReq.open("POST", "/cmd", true);
+    cReq.onload = null
+    cReq.send(name + "=" + value);
 }
 
 function requestFrame() {
-    var oReq = new XMLHttpRequest();
+    if(oReq.readyState !=0 && oReq.readyState !=4 ) return;
     oReq.open("POST", "/frame", true);
-    oReq.responseType = "arraybuffer";
-    oReq.onload = function (oEvent) {
-      var arrayBuffer = oReq.response; // Note: not oReq.responseText
-      if (arrayBuffer && arrayBuffer.byteLength > 2) {
-        var dView = new DataView(arrayBuffer);
-        var header = dView.getUint16(0, true);
-
-        var shortArray = new Uint16Array(arrayBuffer.byteLength / 2 - 1);
-        for(var i = 0; i < shortArray.length;i++)
-            shortArray[i] = dView.getUint16(i * 2 + 2, true);
-        if(shortArray.length >0) {
-            drawData([shortArray]);
-        }
-        document.getElementById("device-info").innerHTML = "";
-      }
-    };
-    oReq.onerror = function (oEvent) {
-        document.getElementById("device-info").innerHTML = "<em>Communication error</em>";
-    };
-    oReq.ontimeout = function (oEvent) {
-        document.getElementById("device-info").innerHTML = "<em>Communication timeout</em>";
-    };
-
     oReq.send(null);
 }
 
