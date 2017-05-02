66707c3dbbf95e3380b7f2cac6f511a670baaaec
elmot <me@elmot.xyz>
2017-05-03 01:58:46 +0300

    Roughly fix line feeds

--- client/src/main/java/xyz/elmot/gencfile/elements/NamedElement.java
+++ client/src/main/java/xyz/elmot/gencfile/elements/NamedElement.java
@@ -18,8 +18,12 @@ public abstract class  NamedElement extends CommonElement{
     @Override
     protected void processSubTag(XMLEventReader reader, StartElement innerElement) throws XMLStreamException {
         if (readText(reader, innerElement, "name", s -> this.name = s)) return;
-        if (readText(reader, innerElement, "description", s -> this.description = s)) return;
+        if (readText(reader, innerElement, "description", s -> this.description = normalize(s))) return;
         super.processSubTag(reader, innerElement);
+    }
+
+    private String normalize(String s) {
+        return s.replace('\n', ' ').replace('\r', ' ');
     }
 
     public String getName() {
