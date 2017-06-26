8db729bb7cba8e19bf385a5de2b591877018f000
elmot <me@elmot.xyz>
2017-06-27 00:03:46 +0300

    NW.js client instead of electron

--- client/hw.js
+++ client/hw.js
@@ -7,39 +7,59 @@ var frameParam = {w: 1400, h: 4096, vertGridN: 14, horGridN: 10};
 
 //var data = [null, null, null, null, null, null];
 
+let port = {path: "/dev/ttyACM0", received: ""};
+
+function convertArrayBufferToString(buf) {
+    let bufView = new Uint8Array(buf);
+    let encodedString = String.fromCharCode.apply(null, bufView);
+    return decodeURIComponent(encodedString);
+}
 
 function setParam(name, value) {
-    osc.sendCommand(name + "=" +  value);
+    osc.sendCommand(name + "=" + value);
 }
 
 function initHW() {
-    let frame = [];
-    const shell = require('child_process');
-    shell.exec("stty -F /dev/ttyACM0 1152000")
-
-    const readline = require('readline');
-    const fs = require('fs');
-    const rl = readline.createInterface({
-        input: fs.createReadStream('/dev/ttyACM0')
-    });
-
-    rl.on('line', function (line) {
-        if (line === "FRAME") {
-            frame = [];
-        } else if (line === "END") {
-            drawData([frame])
-        } else
-        {
-            frame.push(parseInt(line, 16))
+    chrome.serial.getDevices(function (ports) {
+        for (let i = 0; i < ports.length; i++) {
+            console.log(ports[i].path + "; " + ports[i].displayName);
         }
+    });
+    const startString = "FRAME\n";
+    const endString = "\nEND\n";
+    let onReceiveCallback = function (info) {
+            if (info.connectionId === port.connectionId && info.data) {
+                port.received += convertArrayBufferToString(info.data);
+                let endIndex = port.received.indexOf(endString);
+                if (endIndex >= 0) {
+                    let frameIndex = port.received.indexOf(startString);
+                    if (frameIndex >= 0 && frameIndex < endIndex) {
+                        let frameText = port.received.substring(frameIndex + startString.length, endIndex);
+                        frameText = frameText.split(/\n/);
+                        let data = [];
+                        for (let i = 0; i < frameText.length; i++) {
+                            data.push(parseInt(frameText[i],16))
+                        }
+                        drawData([data])
+                    }
+                    port.received = port.received.substr(endIndex + endString.length);
+                }
+            }
+        }
+    ;
+    chrome.serial.onReceive.addListener(onReceiveCallback);
+    chrome.serial.connect(port.path, {bitrate: 1152000}, function (connectionInfo) {
+        // The serial port has been opened. Save its id to use later.
+        port.connectionId = connectionInfo.connectionId;
+        // Do whatever you need to do with the opened port.
     });
 }
 
 function d() {
     document.getElementById("trig.level").value = 200
-    var d = [[], []];
-    var dx = Math.random();
-    for (var i = 0; i < 1400; i++) {
+    let d = [[], []];
+    let dx = Math.random();
+    for (let i = 0; i < 1400; i++) {
         d[1][i] = 4000 * Math.sin(dx / 20 + i / 200);
         d[0][i] = 4000 * Math.sin(dx + i / 250);
     }
